f4f5982c3dffc9790664bf91456c942db91ebaaa
Rodrigovisky <rodrigocampos235@gmail.com>
2026-03-10 19:11:34 -0300

    ex5

--- listaex2/exercicio5.php
+++ listaex2/exercicio5.php
@@ -34,7 +34,7 @@
                 $valor3 = $_POST['nota3'];
                 $media = ($valor1 + $valor2 + $valor3) / 3;
                 
-                echo "A média é:  $media";
+                echo "A média é de:  $media";
             }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
