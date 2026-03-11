59ab5a7fd36005ea636788a37296ef2336d1be3f
Rodrigovisky <rodrigocampos235@gmail.com>
2026-03-10 21:20:14 -0300

    ex11-12

--- exercicio12.php
+++ exercicio12.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Exercicio5</title>
+    <title>Exercicio12</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <div class="container py-3">
-        <h1>Exercicio9</h1>
+        <h1>Exercicio12</h1>
         <form method="post">
             <div class="mb-3">
                 <label for="valor1" class="form-label">Informe o valor</label>
@@ -26,9 +26,9 @@
             {
                 $valor1 = $_POST['valor1'];
                 $valor2 = $_POST['valor2'];
-                $area = $valor1 * $valor2 ** 2;
+                $base = $valor1 * $valor2 ** 2;
                            
-                echo "O resultado é:  $area";
+                echo "O resultado é:  $base";
             }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
