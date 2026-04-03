7bbf9fb4e1f38bb82a8b50fb560bdf335b124216
Rodrigovisky <rodrigocampos235@gmail.com>
2026-04-03 14:37:03 -0300

    ex13-15

--- listaex4/exercicio14.php
+++ listaex4/exercicio14.php
@@ -3,44 +3,33 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 13</title>
+<title>Exercício 14</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container py-3">
-<h1>Exercício 13</h1>
+<h1>Exercício 14</h1>
 <form method="post">
 <div class="mb-3">
-              <label for="frase" class="form-label">Digite uma frase:</label>
-              <input type="text" id="frase" name="frase" class="form-control" required="">
+              <label for="palvra" class="form-label">Digite uma palavra:</label>
+              <input type="text" id="palavra" name="palavra" class="form-control" required="">
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
 <?php
 if($_SERVER['REQUEST_METHOD'] == "POST")
 {
-    $frase = $_POST['frase'];
+    $palavra = $_POST['palavra'];
+    $invertida = strrev($palavra);
 
-    $palavras = explode(" ", trim($frase));
-    $total = 0;
-    $maior = "";
-
-    for($i = 0; $i < count($palavras); $i++)
+    if($palavra == $invertida)
     {
-        if($palavras[$i] != "")
-        {
-            $total++;
-
-            if(strlen($palavras[$i]) > strlen($maior))
-            {
-                $maior = $palavras[$i];
-            }
-        }
+        echo "<p>$palavra -> é palíndromo</p>";
     }
-
-    echo "<p>Frase: $frase</p>";
-    echo "<p>Total de palavras: $total</p>";
-    echo "<p>Maior palavra: $maior</p>";
+    else
+    {
+        echo "<p>$palavra -> não é palíndromo</p>";
+    }
 }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
