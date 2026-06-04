2c17be4634fe4fadb0cb0e1ddb7719e56850c364
Rodrigovisky <rodrigocampos235@gmail.com>
2026-06-04 15:46:10 -0300

    index

--- ProjetoSup/index.php
+++ ProjetoSup/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Controle de Frotas de Táxi</title>
+    <title>Sistema de Controle</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet">
 </head>
@@ -11,7 +11,7 @@
 
 <div class="container d-flex justify-content-center align-items-center vh-100">
     <div class="card shadow p-4" style="width: 100%; max-width: 400px;">
-        <h3 class="text-center mb-4">Sistema de Controle de Frotas de Táxi</h3>
+        <h3 class="text-center mb-4">Sistema de Controle </h3>
 
         <form method="post">
             <div class="mb-3">
